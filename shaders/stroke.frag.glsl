23495e1625ec9e60808057009bdd90262a468f0a
giuliom95 <giuliomartella1995@gmail.com>
2019-05-24 11:59:37 +0200

    Better stroke antialias

--- shaders/stroke.frag.glsl
+++ shaders/stroke.frag.glsl
@@ -1,10 +1,15 @@
 #version 450 core
 
-in vec2 uv;
+in vec3 uvb;
 
 out float stroke;
 
 void main(){
-    float v = 1 - pow(max(0, 2 * length(2*uv - 1) - 1), 4);
+	float bs = 1.5;				// Blur Size
+	float isbs = uvb.z / bs;	// Inverted Scaled Blur Size
+
+	//float v = 1 - pow(max(0, 2 * length(2*uvb.st - 1) - 1), 4);
+	vec2 uv = 2*uvb.st - 1;
+	float v = min(1, -isbs*(length(uv) - 1));
 	stroke = v;
 }
--- shaders/stroke.geom.glsl
+++ shaders/stroke.geom.glsl
@@ -6,31 +6,37 @@ layout(triangle_strip, max_vertices = 8) out;
 uniform ivec2 canvasSize; 
 uniform int brushSize; 
 
-out vec2 uv;
+// UV coords + brush radius;
+out vec3 uvb;
 
 vec4 canvas2view(vec2 p) {
 	return vec4(2 * (p / canvasSize) - 1, 0, 1);
 }
 
 void main() {
+	// Positions
 	vec2 v0 = gl_in[0].gl_Position.xy;
 	vec2 v1 = gl_in[1].gl_Position.xy;
+
+	// Pressures
+	float p0 = gl_in[0].gl_Position.z;
+	float p1 = gl_in[1].gl_Position.z;
+
 	vec2 d = v1 - v0;
 	vec2 dn = dot(d, d) < 0.00001 ? vec2(1,0) : normalize(d);
 	vec2 td = vec2(dn.y, -dn.x);
 
-	float s = brushSize;
-	float s0 = s * gl_in[0].gl_Position.z;
-	float s1 = s * gl_in[1].gl_Position.z;
+	float b0 = brushSize * p0;
+	float b1 = brushSize * p1;
 
-	gl_Position = canvas2view(v0 + s0*td - s0*dn);	uv = vec2(0.0, 0.0); EmitVertex();
-	gl_Position = canvas2view(v0 - s0*td - s0*dn);	uv = vec2(0.0, 1.0); EmitVertex();
-	gl_Position = canvas2view(v0 + s0*td);			uv = vec2(0.5, 0.0); EmitVertex();
-	gl_Position = canvas2view(v0 - s0*td);			uv = vec2(0.5, 1.0); EmitVertex();
-	gl_Position = canvas2view(v1 + s1*td);			uv = vec2(0.5, 0.0); EmitVertex();
-	gl_Position = canvas2view(v1 - s1*td);			uv = vec2(0.5, 1.0); EmitVertex();
-	gl_Position = canvas2view(v1 + s1*td + s1*dn);	uv = vec2(1.0, 0.0); EmitVertex();
-	gl_Position = canvas2view(v1 - s1*td + s1*dn);	uv = vec2(1.0, 1.0); EmitVertex();
+	gl_Position = canvas2view(v0 + b0*td - b0*dn);	uvb = vec3(0.0, 0.0, b0); EmitVertex();
+	gl_Position = canvas2view(v0 - b0*td - b0*dn);	uvb = vec3(0.0, 1.0, b0); EmitVertex();
+	gl_Position = canvas2view(v0 + b0*td);			uvb = vec3(0.5, 0.0, b0); EmitVertex();
+	gl_Position = canvas2view(v0 - b0*td);			uvb = vec3(0.5, 1.0, b0); EmitVertex();
+	gl_Position = canvas2view(v1 + b1*td);			uvb = vec3(0.5, 0.0, b1); EmitVertex();
+	gl_Position = canvas2view(v1 - b1*td);			uvb = vec3(0.5, 1.0, b1); EmitVertex();
+	gl_Position = canvas2view(v1 + b1*td + b1*dn);	uvb = vec3(1.0, 0.0, b1); EmitVertex();
+	gl_Position = canvas2view(v1 - b1*td + b1*dn);	uvb = vec3(1.0, 1.0, b1); EmitVertex();
 
 	EndPrimitive();
 }
